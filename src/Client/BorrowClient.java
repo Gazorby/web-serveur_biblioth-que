db5fd8d3672b7f4e50b2571849e7bc90a72da064
matthieu <mmacnab22@gmail.com>
2018-10-03 19:44:40 +0200

    Project structure

---
+++ src/Client/BorrowClient.java
@@ -0,0 +1,10 @@
+package client;
+
+public class BorrowClient {
+
+    static private final String HOST = "localhost";
+    private static int PORT = 2600;
+
+    public static void main(String[] args) {
+    }
+}
